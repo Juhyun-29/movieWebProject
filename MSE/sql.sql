32a78b1cf31878f440a1671c8d670e71cb2c902a
Juhyun-29 <keprincejhi@naver.com>
2023-05-26 17:54:14 +0900

    dd

--- MSE/sql.sql
+++ MSE/sql.sql
@@ -12,19 +12,19 @@ role varchar(5) default 'User'
 
 drop table movieMember
 select * from movieMember
+alter table movieMember modify mail varchar2(100)
 
 -- 관리자 계정
 insert into movieMember
 values('Admin','admin123','관리자', '관리자', '2023-05-15','관리자','Admin@movieStation.com','Admin');
 
 -- 유저 계정
-insert into movieMember(id, password, name, nickname, birthday, gender,mail)
-values('park','park123','박명수', '바다의왕자','1970-09-27','남성','park@naver.com');
+insert into movieMember(id, password, name, nickname, birthday, gender, mail) values('park','park123','박명수', '바다의왕자','1970-09-27','남성','park@naver.com');
 
 --게시판 테이블 생성
 create table movieBoard(
 seq number(5) primary key,
-subject varchar(30) not null,
+subject varchar2(30) not null,
 title varchar2(200) not null,
 nickname varchar2(30) not null,
 content varchar2(2000) not null,
@@ -32,6 +32,8 @@ regdate date default sysdate,
 cnt number(5) default 0,
 id varchar2(20) constraint movieBoardFK references movieMember(id)
 )
+
+select * from movieBoard
 
 --영화 한줄평 테이블 생성
 create table movieComment(
